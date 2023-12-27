f31f2d0a5eecceb13eb806cc6939b9217a5390c6
Rolin Blake <rolinmblake@gmail.com>
2023-12-27 12:48:43 -0600

    forward euler is working for the most part

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,22 @@ fn main() {
   // Forward Euler Method for example equation:
   // f(t, y) = dy/dt = -y + (2e^(-t) * cos(2t)) where y(0) = 0
   //  -> the exact solution y(t) = e^(-t) * sin(2t)
-  let T = 1.0;
-  let N = 5;
-  let k = T/N;
-  let mut t = vec![0.0, N+1];
-  let mut y = vec![0.0, N+1];
-  let mut sln = vec![0.0, N+1];
-  let mut err = vec![0.0, N+1];
-  y[0] = 0.0;
-  sln[0] = 0.0;
-  for i in range 0..N {
-    t[i+1] = i*k;
-    y[i+1] = y[i]+k*(-1.0*y[i]+2.0*(-1.0*t[i]).exp()*(2.0*t[i]).cos());
-    sln[i+1] = ((-1.0*t[i+1]).exp()*(2.0*t[i+1]).sin());
-    err[i+1] = (sln[i+1]-y[i+1]).abs();
+  let t = 1.0;
+  let n = 50;
+  let k = t/(n as f64);
+  let mut tn: Vec<f64> = vec![0.0; n];
+  let mut yn: Vec<f64> = vec![0.0; n];
+  let mut solution: Vec<f64> = vec![0.0; n];
+  let mut err: Vec<f64> = vec![0.0; n];
+  //yn[0] = 0.0; // technically are already 0.0
+  //solution[0] = 0.0;
+  print!("\nBEFORE ITERATING: tn[0] = 0.0; yn[0] = 0.0; solution[0] = 0.0; err[0] = 0.0\n\n");
+  for i in 0..(n-1) {
+    tn[i+1] = (i as f64)*k;
+    yn[i+1] = yn[i] + k*(-1.0*yn[i] + 2.0*(-1.0*tn[i]).exp() * (2.0*tn[i]).cos() );
+    solution[i+1] = (-1.0*tn[i+1]).exp()*(2.0*tn[i+1]).sin();
+    err[i+1] = (solution[i+1]-yn[i+1]).abs();
+    print!("> i = {}; tn[{}] = {}; yn[{}] = {}; solution[{}] = {}; err[{}] = {}\n", i, i+1, tn[i+1], i+1, yn[i+1], i+1, solution[i+1], i+1, err[i+1]);
   }
+  print!("\nPOST ITERATION RESULT: tn[{}] = {}; yn[{}] = {}; solution[{}] = {}; err[{}] = {}\n", n-1, tn[n-1], n-1, yn[n-1], n-1, solution[n-1], n-1, err[n-1]);
 }
